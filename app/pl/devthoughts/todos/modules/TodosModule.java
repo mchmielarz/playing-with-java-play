32fc9f15666608acd037810c3548e59a778c3d5d
mchmielarz <Michal.Chmielarz@4finance.com>
2016-10-21 18:57:31 +0200

    Unnecessary use of binding annotation removed.

--- app/pl/devthoughts/todos/modules/TodosModule.java
+++ app/pl/devthoughts/todos/modules/TodosModule.java
@@ -1,16 +1,14 @@
 package pl.devthoughts.todos.modules;
 
 import com.google.inject.AbstractModule;
-import com.google.inject.Inject;
-import pl.devthoughts.todos.repository.TodoItemHashMapRepository;
 import pl.devthoughts.todos.repository.TodoItemRepository;
+import pl.devthoughts.todos.repository.TodoItemSqlRepository;
 
-class TodosModule extends AbstractModule {
+public class TodosModule extends AbstractModule {
 
     @Override
     protected void configure() {
         bind(TodoItemRepository.class)
-            .annotatedWith(Inject.class)
-            .to(TodoItemHashMapRepository.class);
+            .to(TodoItemSqlRepository.class);
     }
 }
